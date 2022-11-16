7b91a37f7cca23032bea534797e4172de2b13111
Maxime Austruy <austruym@gmail.com>
2022-11-16 11:11:52 +0100

    remove big lambda

--- src/main.rs
+++ src/main.rs
@@ -188,42 +188,43 @@ impl RenderJob {
         //d > 0.5
     }
 
+    fn calc_one_ray(&self, pmap: &mut HashMap<String,RGB>, u0: f64, v0: f64) -> RGB {
+        let key = format!("{}-{}", u0, v0);
+
+        if self.opt.adaptive_sampling != 0 {
+            if let Some(c) = pmap.get(&key) {
+                return *c;
+            }
+        }
+        let camera = self.camera.as_ref().unwrap();
+        let camera_pos = camera.pos;
+        let camera_dir = camera.dir;
+        let camera_u = camera.screen_u;
+        let camera_v = camera.screen_v;
+        let pixel = camera_pos + camera_dir + camera_u * u0 + camera_v * v0;
+        let ray = Ray{ orig: camera_pos, dir: pixel - camera_pos };
+
+        self.num_rays_sampling.fetch_add(1, Ordering::SeqCst);
+
+        let c = self.calc_ray_color(ray, false, 0);
+        if self.opt.adaptive_sampling != 0 {
+            pmap.insert(key, c);
+        }
+        c
+    }
+
     /*
      * pos_u: -0.5 .. 0.5
      * pos_v: -0.5 .. 0.5
      */
     fn calc_ray_box(&self, pmap: &mut HashMap<String,RGB>, pos_u: f64, pos_v: f64, du: f64, dv: f64, lvl: u32) -> RGB {
-        let mut calc_one_ray = |u0, v0| -> RGB {
-            let key = format!("{}-{}", u0, v0);
-
-            if self.opt.adaptive_sampling != 0 {
-                if let Some(c) = pmap.get(&key) {
-                    return *c;
-                }
-            }
-            let camera = self.camera.as_ref().unwrap();
-            let camera_pos = camera.pos;
-            let camera_dir = camera.dir;
-            let camera_u = camera.screen_u;
-            let camera_v = camera.screen_v;
-            let pixel = camera_pos + camera_dir + camera_u * u0 + camera_v * v0;
-            let ray = Ray{ orig: camera_pos, dir: pixel - camera_pos };
-
-            self.num_rays_sampling.fetch_add(1, Ordering::SeqCst);
-
-            let c = self.calc_ray_color(ray, false, 0);
-            if self.opt.adaptive_sampling != 0 {
-                pmap.insert(key, c);
-            }
-            c
-        };
         if self.opt.adaptive_sampling == 0 {
-            return calc_one_ray(pos_u + du / 2.0, pos_v + dv / 2.0);
+            return self.calc_one_ray(pmap, pos_u + du / 2.0, pos_v + dv / 2.0);
         }
-        let mut c00 = calc_one_ray(pos_u,      pos_v);
-        let mut c01 = calc_one_ray(pos_u,      pos_v + dv);
-        let mut c10 = calc_one_ray(pos_u + du, pos_v);
-        let mut c11 = calc_one_ray(pos_u + du, pos_v + dv);
+        let mut c00 = self.calc_one_ray(pmap, pos_u,      pos_v);
+        let mut c01 = self.calc_one_ray(pmap, pos_u,      pos_v + dv);
+        let mut c10 = self.calc_one_ray(pmap, pos_u + du, pos_v);
+        let mut c11 = self.calc_one_ray(pmap, pos_u + du, pos_v + dv);
 
         if lvl < self.opt.adaptive_max_depth {
             let color_diff = Self::corner_difference(c00, c01, c10, c11);
